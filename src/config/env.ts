b3f59d1c3c0fee440f3e24f381392ec2e951fecd
edwinhati <edwinhati@outlook.com>
2024-09-05 23:30:07 +0700

    chore: remove Sentry

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,13 +4,8 @@ import { createEnv } from "@t3-oss/env-nextjs";
 export const env = createEnv({
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
-    SENTRY_ORG: z.string(),
-    SENTRY_URL: z.string(),
-    SENTRY_PROJECT: z.string(),
-    SENTRY_AUTH_TOKEN: z.string(),
   },
   client: {
-    NEXT_PUBLIC_SENTRY_DSN: z.string(),
     NEXT_PUBLIC_POSTHOG_KEY: z.string(),
     NEXT_PUBLIC_POSTHOG_HOST: z.string(),
     NEXT_PUBLIC_AUTH_API_URL: z.string(),
@@ -18,11 +13,6 @@ export const env = createEnv({
   },
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
-    SENTRY_ORG: process.env.SENTRY_ORG,
-    SENTRY_URL: process.env.SENTRY_URL,
-    SENTRY_PROJECT: process.env.SENTRY_PROJECT,
-    SENTRY_AUTH_TOKEN: process.env.SENTRY_AUTH_TOKEN,
-    NEXT_PUBLIC_SENTRY_DSN: process.env.NEXT_PUBLIC_SENTRY_DSN,
     NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
     NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     NEXT_PUBLIC_AUTH_API_URL: process.env.NEXT_PUBLIC_AUTH_API_URL,
